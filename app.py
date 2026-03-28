b40e115849eb8a23fbbc7515ddc7162d8cd9edde
Pulkit Tiwari <ptiwari@clearwateranalytics.com>
2026-03-28 21:39:43 +0530

    Initial Changes

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit
+from dotenv import load_dotenv
+import base64
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_classic.chains import ConversationalRetrievalChain
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from PyPDF2 import PdfReader, PdfWriter
+from tempfile import NamedTemporaryFile
+
+# Task 1: Import the Libraries
+
+
+# Task 4: Process the Input PDF
+
+
+# Task 6: Method for Handling User Input
+
+
+def main():
+    pass
+
+    # Task 3: Create Web-page Layout
+    "My app"
+
+    # Task 5: Load and Process the PDF
+
+    # Task 7: Handle Query and Display Pages
+
+
+if __name__ == '__main__':
+    main()
+
